f7f9123ab9112888fd1bf4e9d06cd4aa64f9948a
Ahmed Hossam <ahmedhossam114231@gmail.com>
2021-12-22 07:45:52 +0200

    update order $ user

--- app/controllers/OrderCtrl/fetchOne.js
+++ app/controllers/OrderCtrl/fetchOne.js
@@ -7,8 +7,9 @@ module.exports = $baseCtrl(async (req, res) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) return APIResponse.NotFound(res);
     const order = await models._order.findById(id).populate(
-        ["products",
+        [ 
             "result",
+            { path: 'products', populate: { path: 'tests' } },
             { path: "offers", populate: { path: "tests" } }
         ]);
     if (!order) return APIResponse.NotFound(res, "order not found");
